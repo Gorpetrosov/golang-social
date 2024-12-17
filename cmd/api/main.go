7932e7503ff79edc491802c2ba59006ae715d0d2
Gor <gorpetrosyan1991@gmail.com>
2024-12-17 15:43:36 +0400

    feat[main] | db connection and docker compose

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/Gorpetrosov/golang-social/internal/db"
 	"github.com/Gorpetrosov/golang-social/internal/env"
 	"github.com/Gorpetrosov/golang-social/internal/store"
 	"github.com/joho/godotenv"
@@ -14,11 +15,30 @@ func main() {
 	if err != nil {
 	  log.Fatal("Error loading .env file")
 	}
-	store := store.NewPostgresStorage(nil)
 
 	cfg := config{
 		addr: env.GetEnvString("ADDR", ":8000"),
+		db: dbConfig{
+			addr: env.GetEnvString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
+			maxOpenConns: env.GetEnvInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetEnvInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime: env.GetEnvString("DB_MAX_IDLE_TIME", "30m"),
+		},
 	}
+
+    db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxOpenConns, cfg.db.maxIdleTime)
+
+
+	if(err != nil) {
+		log.Panic(err)
+	}
+
+	defer db.Close();
+
+	log.Println("database connection pool established")
+
+	store := store.NewPostgresStorage(db)
+
 	app := &application{
 		config: cfg,
 		store: store,
